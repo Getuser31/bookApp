2f80825007307bc152b57e4f4ae46854d0d470ee
charles fournet <charles.fournet@pm.me>
2024-05-16 11:08:18 +0200

    Add tailwind CSS Framework

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import {svelte} from '@sveltejs/vite-plugin-svelte';
+import commonjs from 'vite-plugin-commonjs';
+import postcss from 'postcss';
+import tailwindcss from 'tailwindcss';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
+        postcss({
+            plugins: [
+                tailwindcss(resolve('./tailwind.config.js')),
+                // autres plugins PostCSS (autoprefixer, etc.)
+            ],
+        }),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
+        svelte(),
+        commonjs()
     ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, '/resources/js'),
+        },
+    },
+    server: {
+        host: 'localhost'
+    }
 });
