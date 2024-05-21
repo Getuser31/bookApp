cae74fb7505f74671bf380ce8a346ca81ec54b8c
charles fournet <charles.fournet@pm.me>
2024-05-21 13:25:10 +0200

    Add Collection model and associate with Book model
    Books are now associated with a collection. A new model 'Collection' was created and foreignKey relation was added to the 'books' table to allow this association. The relationship logic was added to the book and collection models for future querying.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
@@ -14,4 +15,9 @@ class Book extends Model
         'date_of_publication',
         'description',
     ];
+
+    public function collection(): BelongsTo
+    {
+        return $this->belongsTo(Collection::class);
+    }
 }
