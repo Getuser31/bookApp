c0196e3ef538bd63eba385387ab2ed2d6afc1389
charles fournet <charles.fournet@pm.me>
2024-05-22 16:39:05 +0200

    Update GenreFactory to use predefined genres
    The GenreFactory has been updated to generate genres from a predefined array instead of generating random names. This change improves the relevance and usability of the test data being generated by the factory.

--- database/factories/GenreFactory.php
+++ database/factories/GenreFactory.php
@@ -2,13 +2,16 @@
 
 namespace Database\Factories;
 
+use App\Models\Genre;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Genre>
+ * @extends Factory<Genre>
  */
 class GenreFactory extends Factory
 {
+    private array $genre = ['Romance', 'Science Fiction', 'Mystery', 'Drama', 'Action'];
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +20,7 @@ class GenreFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => $this->genre[array_rand($this->genre)],
         ];
     }
 }
