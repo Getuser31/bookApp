364edc0afa1a19ede5bbc0fdfbd1b0151cb9a781
charles fournet <charles.fournet@pm.me>
2024-05-31 11:58:46 +0200

    Add user authentication and login page
    The commit includes the creation of a new UserController for handling user logins alongside a login blade view. A new route for the login page was added as well. An auth check was applied in the BookController to redirect non-authenticated users to the login page. Removed the welcome view which is no longer required.

---
+++ resources/views/User/login.blade.php
@@ -0,0 +1,47 @@
+@extends('index')
+
+@section('content')
+    <div class="container mx-auto">
+        <h2 class="mb-4 text-4xl font-extrabold ">Login Form</h2>
+
+        <div class="w-full max-w-xs">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" action="{{@route('loginPost')}}">
+                @csrf
+                <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                        Email
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        name="email" id="email" type="text" placeholder="Email" value="{{old('email')}}">
+                    @error('email')
+                    <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">{{$message}}</div>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input
+                        class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        name="password" id="password" type="password" placeholder="******************" value="{{old('password')}}">
+                    <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                    @error('password')
+                    <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">{{$message}}</div>
+                    @enderror
+                </div>
+                <div class="flex items-center justify-between">
+                    <button
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit">
+                        Sign In
+                    </button>
+                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                        Forgot Password?
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@endsection
