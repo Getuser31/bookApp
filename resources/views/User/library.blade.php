92f9199f8fa8041189703f75971722e920f01848
Charles <charles.fournet@pm.me>
2024-07-04 21:31:38 +0200

    Add clear filter functionality and refactor filters
    Added a new "Clear Filter" button to easily remove all filters at once. Refactored the filtering functions to separate concerns and handle filters by genre and author independently. The refactoring improves the readability and maintainability of the code.

--- resources/views/User/library.blade.php
+++ resources/views/User/library.blade.php
@@ -6,114 +6,116 @@
         <h1>Welcome to your Library</h1>
         <div class="container mx-auto mt-4">
             <div class="flex justify-end space-x-4 relative">
+                <button id="clearFilter" type="button"
+                        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                    Clear Filter
+                </button>
                 <button id="dropdownCategory" data-dropdown-toggle="dropdown"
                         class="text-blue-500 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                         type="button">
                     Filter by genre
                 </button>
-             <button id="dropdownAuthor" data-dropdown-toggle="dropdown"
-                 class="text-blue-500 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                 type="button">
-                 Filter by author
-         </div>
-                 <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                    </svg>
+                <button id="dropdownAuthor" data-dropdown-toggle="dropdown"
+                        class="text-blue-500 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        type="button">
+                    Filter by author
                 </button>
+            </div>
 
-                <!-- Dropdown menu genre-->
-                <div id="dropdownCategoryMenu" class="z-10 hidden absolute right-0 mt-2 w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
-                    <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
-                        Genre
-                    </h6>
-                    <ul class="space-y-2 text-sm" aria-labelledby="dropdownCategoryMenu">
-                        @foreach($genres as $genre)
-                            <li class="flex items-center">
-                                <input id="{{$genre->name}}" type="checkbox" value="{{$genre->id}}"
-                                       class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 genre-checkbox"/>
+            <!-- Dropdown menu genre-->
+            <div id="dropdownCategoryMenu"
+                 class="z-10 hidden absolute right-0 mt-2 w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
+                <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                    Genre
+                </h6>
+                <ul class="space-y-2 text-sm" aria-labelledby="dropdownCategoryMenu">
+                    @foreach($genres as $genre)
+                        <li class="flex items-center">
+                            <input id="{{$genre->name}}" type="checkbox" value="{{$genre->id}}"
+                                   class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 genre-checkbox"/>
 
-                                <label for="{{$genre->name}}"
-                                       class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-                                    {{$genre->name}}
-                                </label>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-                <!-- Dropdown menu author-->
-                <div id="dropdownAuthorMenu" class="z-10 hidden absolute right-0 mt-2 w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
-                    <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
-                        Authors
-                    </h6>
-                    <ul class="space-y-2 text-sm" aria-labelledby="dropdownAuthorMenu">
-                        @foreach($authors as $author)
-                            <li class="flex items-center">
-                                <input id="{{$author->name}}" type="checkbox" value="{{$author->id}}"
-                                       class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 author-checkbox"/>
-                                <label for="{{$author->name}}"
-                                       class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-                                    {{$author->name}}
-                                </label>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+                            <label for="{{$genre->name}}"
+                                   class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                {{$genre->name}}
+                            </label>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <!-- Dropdown menu author-->
+            <div id="dropdownAuthorMenu"
+                 class="z-10 hidden absolute right-0 mt-2 w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
+                <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                    Authors
+                </h6>
+                <ul class="space-y-2 text-sm" aria-labelledby="dropdownAuthorMenu">
+                    @foreach($authors as $author)
+                        <li class="flex items-center">
+                            <input id="{{$author->name}}" type="checkbox" value="{{$author->id}}"
+                                   class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 author-checkbox"/>
+                            <label for="{{$author->name}}"
+                                   class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                {{$author->name}}
+                            </label>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
-
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="table-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Title</th>
-                    <th scope="col" class="px-6 py-3">Date de publication</th>
-                    <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Auteur</th>
-                    <th scope="col" class="px-6 py-3">Synopsis:</th>
-                    <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Genre</th>
-                    <th scope="col" class="px-6 py-3">Collection</th>
-                    <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Progression</th>
-                    <th scope="col" class="px-6 py-3">Delete</th>
-                </tr>
-                </thead>
-                <tbody id="book-table-body">
-                @foreach($books as $book)
-                    <tr id="book-row-{{$book->id}}" class="border-b border-gray-200 dark:border-gray-700" data-genre-id="{{ $book->genre_id }}">
-                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
-                            data-author-id="{{ $book->author->id }}">
-                            <a href="{{route('book.show',  $book->id)}}"> {{ ($book->title)}}</a></td>
-                        <td class="px-6 py-4">{{ ($book->date_of_publication)}}</td>
-                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{ $book->author->name }}</td>
-                        <td class="px-6 py-4 w-1/4">{{\Illuminate\Support\Str::limit($book->description, 200)}}</td>
-                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{ $book->genre->name }}</td>
-                        <td class="px-6 py-4">{{ $book->collection->name ?? '' }}</td>
-                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{ $book->pivot->progression }}
-                            %
-                        </td>
-                        <td class="px-6 py-4">
-                            <form class="deleteBookForm" data-bookid="{{$book->id}}">
-                                @csrf
-                                @method('DELETE')
-                                <meta name="csrf-token" content="{{ csrf_token() }}">
-                                <input type="hidden" id="bookId" value="{{$book->id}}">
-                                <button type='submit' class="h-8 w-8 text-red-500">
-                                    <svg class="h-8 w-8 text-red-500" viewBox="0 0 24 24" fill="none"
-                                         stroke="currentColor"
-                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                        <polygon
-                                            points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"/>
-                                        <line x1="15" y1="9" x2="9" y2="15"/>
-                                        <line x1="9" y1="9" x2="15" y2="15"/>
-                                    </svg>
-                                </button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
     </div>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="table-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Title</th>
+                <th scope="col" class="px-6 py-3">Date de publication</th>
+                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Auteur</th>
+                <th scope="col" class="px-6 py-3">Synopsis:</th>
+                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Genre</th>
+                <th scope="col" class="px-6 py-3">Collection</th>
+                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Progression</th>
+                <th scope="col" class="px-6 py-3">Delete</th>
+            </tr>
+            </thead>
+            <tbody id="book-table-body">
+            @foreach($books as $book)
+                <tr id="book-row-{{$book->id}}" class="border-b border-gray-200 dark:border-gray-700"
+                    data-genre-id="{{ $book->genre_id }}" data-author-id="{{ $book->author->id }}">
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        <a href="{{route('book.show',  $book->id)}}"> {{ ($book->title)}}</a></td>
+                    <td class="px-6 py-4">{{ ($book->date_of_publication)}}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{ $book->author->name }}</td>
+                    <td class="px-6 py-4 w-1/4">{{\Illuminate\Support\Str::limit($book->description, 200)}}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{ $book->genre->name }}</td>
+                    <td class="px-6 py-4">{{ $book->collection->name ?? '' }}</td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{ $book->pivot->progression }}
+                        %
+                    </td>
+                    <td class="px-6 py-4">
+                        <form class="deleteBookForm" data-bookid="{{$book->id}}">
+                            @csrf
+                            @method('DELETE')
+                            <meta name="csrf-token" content="{{ csrf_token() }}">
+                            <input type="hidden" id="bookId" value="{{$book->id}}">
+                            <button type='submit' class="h-8 w-8 text-red-500">
+                                <svg class="h-8 w-8 text-red-500" viewBox="0 0 24 24" fill="none"
+                                     stroke="currentColor"
+                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <polygon
+                                        points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"/>
+                                    <line x1="15" y1="9" x2="9" y2="15"/>
+                                    <line x1="9" y1="9" x2="15" y2="15"/>
+                                </svg>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
     <!-- Render pagination links -->
     {{ $books->links('vendor.pagination.tailwind') }}
 
@@ -184,48 +186,57 @@
             }
         });
         // Filtering Menu
-        </script>
-    <script> //Filter by genre
-        document.addEventListener('DOMContentLoaded', function() {
-            const checkboxes = document.querySelectorAll('.genre-checkbox');
+    </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const genreCheckboxes = document.querySelectorAll('.genre-checkbox');
+            const authorCheckboxes = document.querySelectorAll('.author-checkbox');
+            const clearFilters = document.getElementById('clearFilter');
 
-            checkboxes.forEach(checkbox => {
+            clearFilters.addEventListener('click', resetFilters);
+
+            function resetFilters() {
+                document.querySelectorAll('#book-table-body tr')
+                    .forEach(row => {
+                        row.style.display = '';
+                    })
+
+                genreCheckboxes.forEach(checkbox => {
+                    checkbox.checked = false;
+                });
+
+                authorCheckboxes.forEach(checkbox => {
+                    checkbox.checked = false;
+                })
+            }
+
+            let selectedGenres = [];
+            let selectedAuthors = [];
+
+            genreCheckboxes.forEach(checkbox => {
+                checkbox.addEventListener('change', filterBooks);
+            });
+
+            authorCheckboxes.forEach(checkbox => {
                 checkbox.addEventListener('change', filterBooks);
             });
 
             function filterBooks() {
-                const selectedGenres = Array.from(checkboxes)
+                selectedGenres = Array.from(genreCheckboxes)
+                    .filter(checkbox => checkbox.checked)
+                    .map(checkbox => checkbox.value);
+
+                selectedAuthors = Array.from(authorCheckboxes)
                     .filter(checkbox => checkbox.checked)
                     .map(checkbox => checkbox.value);
 
                 document.querySelectorAll('#book-table-body tr')
                     .forEach(row => {
                         const genreId = row.getAttribute('data-genre-id');
-                        if (selectedGenres.length === 0 || selectedGenres.includes(genreId)) {
-                            row.style.display = '';
-                        } else {
-                            row.style.display = 'none';
-                        }
-                    });
-            }
-        });
-        //Filter by author
-        document.addEventListener('DOMContentLoaded', function() {
-            const checkboxes = document.querySelectorAll('.author-checkbox');
-
-            checkboxes.forEach(checkbox => {
-                checkbox.addEventListener('change', filterBooks);
-            });
-
-            function filterBooks() {
-                const selectedAuthors = Array.from(checkboxes)
-                    .filter(checkbox => checkbox.checked)
-                    .map(checkbox => checkbox.value);
-
-                document.querySelectorAll('#book-table-body tr')
-                    .forEach(row => {
                         const authorId = row.getAttribute('data-author-id');
-                        if (selectedAuthors.length === 0 || selectedAuthors.includes(authorId)) {
+
+                        if ((!selectedGenres.length || selectedGenres.includes(genreId)) &&
+                            (!selectedAuthors.length || selectedAuthors.includes(authorId))) {
                             row.style.display = '';
                         } else {
                             row.style.display = 'none';
