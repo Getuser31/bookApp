c95efaa46a7183ad9045b7c97da285717800c6a0
charles fournet <charles.fournet@pm.me>
2024-09-20 16:12:21 +0200

    Add Telescope assets to public/vendor directory
    This commit introduces the favicon.ico and app.css files to the Telescope assets in the public/vendor directory. These files are necessary for proper styling and favicon functionality using Bootstrap 4.6.2.

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,12 +2,46 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\DefaultLanguage;
+use App\Models\UserPreference;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class UserController
 {
-    public function login(Request $request): \Illuminate\Http\JsonResponse
+    
+    public function login(Request $request): JsonResponse
     {
-        return response()->json(['message' => 'Login successful'], 200);
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            $user = Auth::user();
+            $role = $user->checkAdmin();
+            $userPreference = UserPreference::getUserPreference($user->id);
+            if(!$userPreference){
+                $defaultLanguage = DefaultLanguage::first();
+                UserPreference::create([
+                    'user_id' => $user->id
+                ]);
+            } else {
+                $defaultLanguage = $userPreference->defaultLanguage;
+            }
+            Session::put('admin', $role);
+            Session::put('language', $defaultLanguage->language);
+
+            // Create Sanctum Token
+            $deviceName = $request->userAgent(); // Or a custom device identifier
+            $abilities = ['*']; // Grant all permissions or customize
+            $token = $user->createToken($deviceName, $abilities)->plainTextToken;
+
+            // Store Token in Session
+            Session::put('api_token', $token);
+            return response()->json(['success' => 'login successful'], 200);
+        } else {
+            return Response()->json(['error' => 'Wrong username or password'], 401);
+        }
     }
 }
